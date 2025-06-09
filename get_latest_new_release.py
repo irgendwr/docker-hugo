0f36d8eadda24f83ee43e2ae588299017b944013
Jannick Kremer <jannick.kremer@mailbox.org>
2025-06-09 20:00:52 +0900

    Retrieve our tags from Docker Hub, stop using Github Tags

--- get_latest_new_release.py
+++ get_latest_new_release.py
@@ -10,40 +10,40 @@ def make_comparable(version_string: str) -> list[int]:
 
 
 ENDPOINT_HUGO_IMAGES = "https://ghcr.io/v2/gohugoio/hugo/tags/list"
-ENDPOINT_MY_TAGS = (
-    "https://api.github.com/repos/DeinAlptraum/docker-hugo/tags?per_page=1"
+ENDPOINT_OUR_TAGS = (
+    "https://hub.docker.com/v2/namespaces/nightoo/repositories/hugo/tags"
 )
 
-request_counter = 1
+our_tags = set()
+try_next_page = True
+current_url = ENDPOINT_OUR_TAGS
+while try_next_page:
+    print("Retrieving tags at", current_url, file=stderr)
+    content = requests.get(current_url).json()
+    our_tags = our_tags.union([image["name"] for image in content["results"]])
 
-# Check the latest release we've done
-res = requests.get(ENDPOINT_MY_TAGS)
-try:
-    latest_tag = res.json()[0]["name"]
-    print("Checking for releases older than", latest_tag, file=stderr)
-except IndexError:
-    latest_tag = "v1.0.0.0"
-    print("No tags found, starting with newest Hugo release", file=stderr)
-
+    current_url = content["next"]
+    try_next_page = bool(current_url)
 
 auth = b64encode(os.environ["GH_TOKEN"].encode()).decode()
 res = requests.get(ENDPOINT_HUGO_IMAGES, headers={"Authorization": f"Bearer {auth}"})
 
 version_regex = re.compile("v([0-9]+\\.){2}[0-9]+")
-releases_to_process = [
-    tag
+available_tags = set([
+    tag[1:]
     for tag in res.json()["tags"]
-    if version_regex.match(tag) and make_comparable(tag) < make_comparable(latest_tag)
-]
+    if version_regex.match(tag)
+])
+tags_to_process = available_tags - our_tags
 
-print("Found releases:", file=stderr)
-print(releases_to_process, file=stderr)
+print("Unprocessed releases:", file=stderr)
+print(tags_to_process, file=stderr)
 
-if releases_to_process:
-    minimum_new_version = max(releases_to_process, key=make_comparable)
-    print("Preparing image for", minimum_new_version, file=stderr)
+if tags_to_process:
+    maximum_new_version = max(tags_to_process, key=make_comparable)
+    print("Preparing image for", maximum_new_version, file=stderr)
 else:
-    minimum_new_version = "NONE"
+    maximum_new_version = "NONE"
     print("No new release found, stopping.", file=stderr)
 
-print("version=", minimum_new_version.replace("v", ""), sep="")
+print("version=", maximum_new_version, sep="")
