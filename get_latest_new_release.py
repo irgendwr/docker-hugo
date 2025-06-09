f906cc4c83c96f6c4bdb2e69daac9ca1b0c9f2a0
Jannick Kremer <jannick.kremer@mailbox.org>
2025-06-09 18:58:15 +0900

    Address review comments
    - build newest unbuilt release instead of oldest since v0.88.1
    - write Python logging to stderr and use stdout for version output
    - check available versions via Hugo repository's packages instead of releases

--- get_latest_new_release.py
+++ get_latest_new_release.py
@@ -1,10 +1,18 @@
 import requests
+import os
+from base64 import b64encode
+import re
+from sys import stderr
+
 
 def make_comparable(version_string: str) -> list[int]:
     return [int(part) for part in version_string.replace("v", "").split(".")]
 
-ENDPOINT_HUGO_RELEASES = "https://api.github.com/repos/gohugoio/hugo/releases"
-ENDPOINT_MY_TAGS = "https://api.github.com/repos/DeinAlptraum/docker-hugo/tags?per_page=1"
+
+ENDPOINT_HUGO_IMAGES = "https://ghcr.io/v2/gohugoio/hugo/tags/list"
+ENDPOINT_MY_TAGS = (
+    "https://api.github.com/repos/DeinAlptraum/docker-hugo/tags?per_page=1"
+)
 
 request_counter = 1
 
@@ -12,48 +20,30 @@ request_counter = 1
 res = requests.get(ENDPOINT_MY_TAGS)
 try:
     latest_tag = res.json()[0]["name"]
+    print("Checking for releases older than", latest_tag, file=stderr)
 except IndexError:
-    latest_tag = "v0.88.1"
-print("Checking for releases younger than", latest_tag)
+    latest_tag = None
+    print("No tags found, starting with newest Hugo release", file=stderr)
 
-releases_to_process = []
-try_next_page = True
-current_url = ENDPOINT_HUGO_RELEASES
-while try_next_page:
-    print("Retrieving releases at", current_url)
-    if request_counter < 10:
-        res = requests.get(current_url)
-    else:
-        print("Reached 10 requests")
-        exit(1)
-    request_counter += 1
-    content = res.json()
-    for release in content:
-        # Only check next page if the last release was still newer than our latest
-        try_next_page = False
-        if make_comparable(latest_tag) < make_comparable(release["name"]):
-            try_next_page = True
-            # Remove the "v" at the start of the tag name
-            releases_to_process.append(release["name"][1:])
-        
-    if try_next_page:
-        # Retrieve link to next page from Link header
-        pagination_urls = res.headers["Link"]
-        for element in pagination_urls.split(","):
-            if 'rel="next"' in element:
-                current_url = element.split(">")[0].split("<")[1]
-                break
 
-print("Found releases:")
-print(releases_to_process)
+auth = b64encode(os.environ["GH_TOKEN"].encode()).decode()
+res = requests.get(ENDPOINT_HUGO_IMAGES, headers={"Authorization": f"Bearer {auth}"})
+
+version_regex = re.compile("v([0-9]+\\.){2}[0-9]+")
+releases_to_process = [
+    tag
+    for tag in res.json()["tags"]
+    if version_regex.match(tag) and make_comparable(tag) < make_comparable(latest_tag)
+]
+
+print("Found releases:", file=stderr)
+print(releases_to_process, file=stderr)
 
 if releases_to_process:
-    minimum_new_version = min(releases_to_process, key=make_comparable)
-    print("Preparing image for", minimum_new_version)
+    minimum_new_version = max(releases_to_process, key=make_comparable)
+    print("Preparing image for", minimum_new_version, file=stderr)
 else:
     minimum_new_version = "NONE"
-    print("No new release found, stopping.")
+    print("No new release found, stopping.", file=stderr)
 
-with open("_version.txt", "w") as f:
-    f.write(minimum_new_version)
-
+print(minimum_new_version.replace("v", ""))
